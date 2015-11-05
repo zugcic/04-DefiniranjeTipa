d54bf3a8df8036337a89a7059a78dd489e52c267
julijan <julijan.sribar@gmail.com>
2015-11-05 18:26:09 +0100

    StatičkiČlanovi project modified

--- StatickiClanovi/StatičkiČlanovi.cs
+++ StatickiClanovi/StatičkiČlanovi.cs
@@ -1,9 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Vsite.CSharp
 {
+    class MojaKlasa
+    {
+        static int _brojač = 0;
+
+        // TODO: U konstruktor dodati naredbu koja će uvećati _brojač za 1
+
+        public MojaKlasa()
+        {
+        }
+
+        // TODO: U metodu dodati naredbu koja će ispisati trenutnu vrijednost člana _brojač
+
+        public static void IspišiBrojInstanci()
+        {
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
