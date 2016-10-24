8031d0940390db2a9fce2c8ac19530a211e89717
Julijan Šribar <julijan.sribar@gmail.com>
2016-10-24 23:25:10 +0200

    Prepravljen projekt GCCollect.

--- GCCollect/GCCollect.cs
+++ GCCollect/GCCollect.cs
@@ -5,20 +5,23 @@ namespace Vsite.CSharp
 {
     class Program
     {
+        // TODO: Prekopirati definiciju klase s destruktorom iz prethodnog primjera
+        public class KlasaSDestruktorom
+        {
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Ušao sam u 'Main'");
 
             for (int i = 0; i < 1000; i++)
-            // ako stavimo veliki broj objekata, GC će u jednom trenutku 
-            // početi oslobađati memoriju za nove objekte
-            //for (int i = 0; i < 100000; i++)
             {
                 KlasaSDestruktorom ksd = new KlasaSDestruktorom();
             }
 
-            Console.WriteLine("*** Izlazimo iz bloka ***");
-            Debug.WriteLine("*** Izlazimo iz bloka ***");
+            Console.WriteLine("*** Pozivamo GC.Collect() ***");
+            Debug.WriteLine("*** Pozivamo GC.Collect() ***");
             // explicitno pozivamo sustav za skupljanje smeća
             // redoslijed uništavanja objekata nije unaprijed definiran!
             GC.Collect();
@@ -26,7 +29,7 @@ namespace Vsite.CSharp
             Debug.WriteLine("*** GOTOVO!!! ***");
 
             Console.WriteLine("GOTOVO!!!");
-            Console.ReadLine();
+            Console.ReadKey();
 
             Debug.WriteLine("*** SADA JE KONAČNO!!! ***");
         }
