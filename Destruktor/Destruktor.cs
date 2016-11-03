23227953913026dda8f924fa449b98efda818613
Stjepan Zugcic <stzugcic@vsite.hr>
2016-11-03 19:44:02 +0100

    Riješili smo GCCollect

--- Destruktor/Destruktor.cs
+++ Destruktor/Destruktor.cs
@@ -17,8 +17,8 @@ namespace Vsite.CSharp
 
         // Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
         ~KlasaSDestruktorom() {
-            Console.WriteLine($"Destruktor objekta br. {RedniBroj}.");
-            Debug.WriteLine($"Destruktor objekta br. {RedniBroj}.");
+            Console.WriteLine($"Destruktor objekta br. {RedniBroj}");
+            Debug.WriteLine($"Destruktor objekta br. {RedniBroj}");
         }
 
         public readonly int RedniBroj; // redni broj objekta
--- GCCollect/GCCollect.cs
+++ GCCollect/GCCollect.cs
@@ -5,11 +5,30 @@ namespace Vsite.CSharp
 {
     class Program
     {
-        // TODO: Prekopirati definiciju klase s destruktorom iz prethodnog primjera
+        //  Prekopirati definiciju klase s destruktorom iz prethodnog primjera
         // TODO: Staviti točku prekida (breakpoint) u destruktor, pokrenuti program i pogledati zadnje ispise prije prekida izvođenja.
         public class KlasaSDestruktorom
         {
+            public KlasaSDestruktorom()
+            {
+                RedniBroj = ++Brojač;
+                string poruka = string.Format("Konstruktor objekta br. {0}", RedniBroj);
+                // ispis u konzolu
+                Console.WriteLine(poruka);
+                // dijagnostički ispis u Output prozor Visual Studija
+                Debug.WriteLine(poruka);
+            }
 
+            // Dodati destruktor (tj. finalizirajuću metodu) i njemu napisati naredbe koje će u konzolu i u Output prozor ispisati: "Destruktor objekta br. {RedniBroj}."
+            ~KlasaSDestruktorom()
+            {
+                Console.WriteLine($"Destruktor objekta br. {RedniBroj}");
+                Debug.WriteLine($"Destruktor objekta br. {RedniBroj}");
+            }
+
+            public readonly int RedniBroj; // redni broj objekta
+
+            static int Brojač = 0; // brojač ukupno stvorenih objekata
         }
 
         static void Main(string[] args)
